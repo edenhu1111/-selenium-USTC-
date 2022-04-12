bd08aabdafe59f210a99de67d80e439fcd1d4f92
EdenHU <43788681+edenhu1111@users.noreply.github.com>
2022-04-12 23:28:24 +0800

    Update wlt.py

--- wlt.py
+++ wlt.py
@@ -5,8 +5,8 @@ from time import sleep
 driver = webdriver.Chrome(executable_path="C:\chromedriver\chromedriver.exe")
 driver.get("http://wlt.ustc.edu.cn/cgi-bin/ip")
 
-driver.find_element_by_name('name').send_keys('edenhu1111')
-driver.find_element_by_name('password').send_keys('Edenisme02')
+driver.find_element_by_name('name').send_keys('yourusername')
+driver.find_element_by_name('password').send_keys('yourpassword')
 driver.find_element_by_name("set").click()
 
 sleep(0.5)
